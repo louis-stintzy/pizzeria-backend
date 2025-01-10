f411f6295f6efe82ee7cf760b3648297a5ea6604
louis-stintzy <louis.stintzy@gmail.com>
2025-01-10 13:56:03 +0000

    chore: add CORS package and configure middleware for JSON parsing and URL encoding

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,18 @@
 import express, { Request, Response } from 'express';
+import cors from 'cors';
 import dotenv from 'dotenv';
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.use(
+  // à configurer + ajouter middleware pour personnalisé ( if (req.headers !== process.env.CORS_ORIGIN) {)...)
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true, // allow session cookie from browser to pass through
+  })
+);
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Welcome to the Pizzeria Backend !');
