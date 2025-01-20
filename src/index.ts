5b1b14095ccc30c97792c0dc42a08ceb3def5ee2
louis-stintzy <louis.stintzy@gmail.com>
2025-01-20 10:57:26 +0000

    refactor : split index.ts into two files index.ts / app.ts

--- src/index.ts
+++ src/index.ts
@@ -1,43 +1,6 @@
-import express, { Request, Response } from 'express';
 import { PORT } from './dotenv/config';
-import cors from 'cors';
-import router from './routes';
-import { errorHandler } from './middlewares/errorHandler';
-import { NotFoundError } from './errors';
 
-// todo : scinder en plusieurs fichiers : index.ts & app.ts
-
-const app = express();
-
-app.use(
-  // todo : à configurer + ajouter middleware pour personnalisé ( if (req.headers !== process.env.CORS_ORIGIN) {)...)
-  cors({
-    origin: process.env.CORS_ORIGIN,
-    credentials: true, // allow session cookie from browser to pass through
-  })
-);
-
-// Analyser les requêtes entrantes avec des données JSON (remplace body-parser)
-app.use(express.json());
-
-// Permettre l'utilisation de la methode POST
-app.use(express.urlencoded({ extended: true }));
-
-// Route '/', route de bienvenue
-app.get('/', (_: Request, res: Response) => {
-  res.send('Welcome to the Pizzeria Backend !');
-});
-
-// Routes '/api', router
-app.use('/api', router);
-
-// Routes non définie
-app.use((req: Request, _res: Response) => {
-  throw new NotFoundError({ publicMessage: 'Route not found', internalMessage: `Route ${req.originalUrl} not found` });
-});
-
-// Middleware pour gérer les erreurs
-app.use(errorHandler);
+import app from './app';
 
 // Lance le serveur
 app.listen(PORT, () => {
